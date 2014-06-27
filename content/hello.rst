950099ca7010870dd49fb4aa5c393d3c2fc163fe
Rituparna Panda <rituparna.panda@openlabs.co.in>
2014-06-27 19:02:07 +0530

    Add profile image

--- content/hello.rst
+++ content/hello.rst
@@ -3,6 +3,7 @@ Hello World
 
 :date: 2010-06-27
 :author: Rituparna Panda
+:author_gravatar: https://secure.gravatar.com/avatar/ae41f3e136bbd391562510f7d53f0282?s=100
 :summary: Hello World
 
 This is my first pelican blog.
--- pelicanconf.py
+++ pelicanconf.py
@@ -33,5 +33,6 @@ DEFAULT_PAGINATION = 10
 # Uncomment following line if you want document-relative URLs when developing
 #RELATIVE_URLS = True
 
+PROFILE_IMAGE_URL = 'images/profile.jpg'
 PATH = 'content'
 THEME = 'pure-theme'
