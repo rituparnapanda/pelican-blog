95f1244ddd6d24da57a0b93b2f89b4c727e794cc
Rituparna Panda <rituparna.panda@openlabs.co.in>
2014-06-27 18:49:43 +0530

    First working version of the module

---
+++ content/hello.rst
@@ -0,0 +1,8 @@
+Hello World
+===========
+
+:date: 2010-06-27
+:author: Rituparna Panda
+:summary: Hello World
+
+This is my first pelican blog.
---
+++ pelicanconf.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+from __future__ import unicode_literals
+
+AUTHOR = u'rituparna'
+SITENAME = u'rituparna.com'
+SITEURL = ''
+
+TIMEZONE = 'Asia/Kolkata'
+
+DEFAULT_LANG = u'en'
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+
+# Blogroll
+LINKS =  (
+    ('Python.org', 'http://python.org/'),
+    ('Jinja2', 'http://jinja.pocoo.org/'),
+    ('You can modify those links in your config file', '#'),
+)
+
+# Social widget
+SOCIAL = (
+    ('Twitter', 'https://twitter.com/rituparna02'),
+    ('Linkedin', 'http://in.linkedin.com/in/rituparna02'),
+)
+
+DEFAULT_PAGINATION = 10
+
+# Uncomment following line if you want document-relative URLs when developing
+#RELATIVE_URLS = True
+
+PATH = 'content'
+THEME = 'pure-theme'
